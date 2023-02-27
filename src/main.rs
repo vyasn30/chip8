59aaa9e7b76570d81e2380909a64272fc8e107ec
nate_d1azzz <nate@pop-os.localdomain>
2023-02-27 19:09:09 +0530

    caller added

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ struct CPU {
     registers: [u8; 16],
     program_counter: usize,
     memory: [u8; 0x1000],
+    stack: [u16; 16],
+    stack_pointer: usize,
 }
 impl CPU {
     fn read_opcode(&self) -> u16 {
@@ -10,6 +12,31 @@ impl CPU {
         let opcode_byte1: u16 = self.memory[p + 1] as u16;
 
         return opcode_byte0 << 8 | opcode_byte1;
+    }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow!")
+        }
+
+        stack[sp] = self.program_counter as u16;
+        self.stack_pointer += 1;
+
+        self.program_counter = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow")
+        }
+
+        self.stack_pointer -= 1;
+
+        let call_addr = self.stack[self.stack_pointer];
+        self.program_counter = call_addr as usize;
     }
 
     fn run(&mut self) {
@@ -30,6 +57,11 @@ impl CPU {
                     return;
                 }
                 (0x8, _, _, 0x4) => self.add(x, y),
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => {
+                    let nnn = opcode & (0x0FFF);
+                    self.call(nnn);
+                }
                 _ => todo!("baaki chhe {:04x}", opcode),
             }
         }
@@ -55,24 +87,31 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         program_counter: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
     let mem = &mut cpu.memory;
+    mem[0x000] = 0x21;
+    mem[0x001] = 0x00;
+    mem[0x002] = 0x21;
+    mem[0x003] = 0x00;
+    mem[0x004] = 0x00;
+    mem[0x005] = 0x00;
 
-    mem[0] = 0x80;
-    mem[1] = 0x14;
-    mem[2] = 0x80;
-    mem[3] = 0x24;
-    mem[4] = 0x80;
-    mem[5] = 0x34;
+    mem[0x100] = 0x80;
+    mem[0x101] = 0x14;
+    mem[0x102] = 0x80;
+    mem[0x103] = 0x14;
+    mem[0x104] = 0x00;
+    mem[0x105] = 0xEE;
+
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
-
+    assert_eq!(cpu.registers[0], 45);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
     println!("{}", cpu.registers[0]);
 }
